da78c85984f42041375de8a3effc9795d9821147
cnbactivities@gmail.com <cnbactivities@gmail.com>
2024-03-08 10:30:29 +0700

    tao thiep

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Cầm';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Gửi đến Cầm';
+const contentCard = 'Ngày 8/3, chúc một nửa thế giới luôn thành công trong cuộc sống! Chúc bạn luôn duyên dáng và xinh đẹp trong mắt một nửa thế giới còn lại!';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
